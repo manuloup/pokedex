893e7c761fb7320dda2ff4ca5f592d17aca9e841
manuloup <galland.manuloup@gmail.com>
2026-01-05 15:57:41 +0100

    react basics 02

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,12 @@
+export default function PokemonCard() {
+    return (
+        <figure>
+            Pokemon Card
+            <img
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+                alt="Bulbasaure"
+            />
+            <figcaption>Bulbasaure</figcaption>
+        </figure>
+    );
+}
