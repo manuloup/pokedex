a26120d158fcf65808a2858fdb0b63f62a6da962
manuloup <galland.manuloup@gmail.com>
2026-01-09 11:40:38 +0100

    react basics 06

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
@@ -7,18 +8,31 @@ const pokemonList = [
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
-  {
-    name: "mew",
-  },
+  { name: "mew" },
 ];
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+  const pokemon = pokemonList.find((p) => p.name === pokemonName);
+
+  if (!pokemon) {
+    throw new Error("Invalid pokemon name");
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <button onClick={() => setPokemonName("bulbasaur")}>
+        bulbasaur
+      </button>
+
+      <button onClick={() => setPokemonName("mew")}>
+        mew
+      </button>
+
+      <PokemonCard pokemon={pokemon} />
     </div>
   );
 }
 
 export default App;
-
